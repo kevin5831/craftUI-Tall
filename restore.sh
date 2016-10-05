13e49225040a09768226966bc2ad03b46e2114ef
Andrii <jastinmax999@gmail.com>
2016-10-05 03:49:05 +0300

    Feat: Refactor user authentication components for better maintainability

--- restore.sh
+++ restore.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2015-03-3"  # YYYY-MM-DD
-END_DATE="2015-03-24"    # YYYY-MM-DD
+START_DATE="2016-10-3"  # YYYY-MM-DD
+END_DATE="2016-10-28"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=2
 MAX_COMMITS=6
 
 # Min and max days per month to commit
-MIN_DAYS_PER_MONTH=13
-MAX_DAYS_PER_MONTH=18
+MIN_DAYS_PER_MONTH=5
+MAX_DAYS_PER_MONTH=8
 
 # Set author and committer name and email
 AUTHOR_NAME="Andrii"
