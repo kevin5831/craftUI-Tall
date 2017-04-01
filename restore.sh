469462153047f2a8b4e17fa5c3cf57eb7337839c
Andrii <jastinmax999@gmail.com>
2017-04-01 10:46:39 +0300

    Feat: Add Google login support to authentication UI

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2017-4-3"  # YYYY-MM-DD
-END_DATE="2017-4-28"    # YYYY-MM-DD
+START_DATE="2017-04-3"  # YYYY-MM-DD
+END_DATE="2017-04-28"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=2
